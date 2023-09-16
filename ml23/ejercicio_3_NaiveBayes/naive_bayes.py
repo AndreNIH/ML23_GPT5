5c1b959bbf340460b810a129c5087688fc82d2e9
Dahir <dahirvjc@proton.me>
2023-09-15 19:32:43 -0700

    merge loquesea

--- ml23/ejercicio_3_NaiveBayes/naive_bayes.py
+++ ml23/ejercicio_3_NaiveBayes/naive_bayes.py
@@ -9,23 +9,15 @@ class NaiveBayes():
 
     def fit(self, X, y):
         # TODO: Calcula la probabilidad de que una muestra sea positiva P(y=1)
-<<<<<<< HEAD
         self.prior_positive = sum(n for n in y)/len(y)
         
         # TODO: Calcula la probabilidad de que una muestra sea negativa P(y=0)
         self.prior_negative = 1-self.prior_positive
-=======
-        # self.prior_positives = 
-
-        # TODO: Calcula la probabilidad de que una muestra sea negativa P(y=0)
-        # self.prior_negative = 
->>>>>>> upstream/master
 
         # TODO: Para cada palabra del vocabulario x_i
         # calcula la probabilidad de: P(x_i| y=1)
         # Guardalas en un arreglo de numpy:
         # self._likelihoods_positives = [P(x_1| y=1), P(x_2| y=1), ..., P(x_n| y=1)]
-<<<<<<< HEAD
 
         #P(y|X)=(P(X|y)*P(y))/P(X)
         # TODO:  Para cada palabra del vocabulario x_i, calcula P(x_i| y=0)
@@ -47,22 +39,12 @@ class NaiveBayes():
                 self._likelihoods_negatives += sentence
         self._likelihoods_positives /= positiveWords
         self._likelihoods_negatives /= negativeWords
-=======
-        # self._likelihoods_positives = 
-        
-        # TODO:  Para cada palabra del vocabulario x_i, calcula P(x_i| y=0)
-        # Guardalas en un arreglo de numpy:
-        # self._likelihoods_negatives = [P(x_1| y=0), P(x_2| y=0), ..., P(x_n| y=0)]
-
-        # self._likelihoods_negatives = _likelihoods_negatives
->>>>>>> upstream/master
         return self
 
     def predict(self, X):
         # TODO: Calcula la distribución posterior para la clase 1 dado los nuevos puntos X
         # utilizando el prior y los likelihoods calculados anteriormente
         # P(y = 1 | X) = P(y=1) * P(x1|y=1) * P(x2|y=1) * ... * P(xn|y=1)
-<<<<<<< HEAD
         
 
         pred = []
@@ -96,18 +78,6 @@ class NaiveBayes():
             pred.append(pMult > nMult) #Clasificacion
         return pred
         '''
-=======
-        # posterior_positive = 
-
-        # TODO: Calcula la distribución posterior para la clase 0 dado los nuevos puntos X
-        # utilizando el prior y los likelihoods calculados anteriormente
-        # P(y = 0 | X) = P(y=0) * P(x1|y=0) * P(x2|y=0) * ... * P(xn|y=0)
-        # posterior_negative = 
-
-        # TODO: Determina a que clase pertenece la muestra X dado las distribuciones posteriores
-        # clase = 
-        return
->>>>>>> upstream/master
     
     def score(self, X, y):
         y_pred = self.predict(X)
